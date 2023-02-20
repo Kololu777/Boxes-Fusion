be14ad80af7bbdfc9e4ec37cd712c9c033661051
ko <kosiynon31kos@gmail.com>
2023-02-20 23:06:51 +0900

    add

--- boxes_fusion/translate/memo.md
+++
@@ -1,5 +0,0 @@
-- xyxy 
-  -  xmin ymin xmax ymax
-- cxcywh
-  -  center_x center_y width height
-
--- boxes_fusion/translate/xyxy.py
+++ boxes_fusion/translate/xyxy.py
@@ -3,8 +3,8 @@ import numpy as np
 def xyxy2cxcywh(boxes:np.ndarray)-> np.ndarray:
     assert boxes.shape[-1] == 4 , "The number of dimension must be 4."
     assert len(boxes.shape) == 2, "2d" 
-    boxes[:, 2] = boxes[:, 0] - boxes[:, 2]     # width
-    boxes[:, 3] = boxes[:, 3] - boxes[:, 1]     # hegiht
+    boxes[:, 2] = boxes[:, 0] - boxes[:, 2]         # width
+    boxes[:, 3] = boxes[:, 3] - boxes[:, 1]         # hegiht
     boxes[:, 0] = boxes[:, 0] + (boxes[:, 2] / 2.0) # center_x
     boxes[:, 1] = boxes[:, 1] + (boxes[:, 3] / 2.0) # center_y
     return boxes
@@ -12,10 +12,37 @@ def xyxy2cxcywh(boxes:np.ndarray)-> np.ndarray:
 def cxcywh2xyxy(boxes:np.ndarray)-> np.ndarray:
     assert boxes.shape[-1] == 4 , "The number of dimension must be 4."
     assert len(boxes.shape) == 2, "2d" 
-    boxes[:, 0] = boxes[0] - (boxes[:, 2] / 2.0)
-    boxes[:, 1] = boxes[1] - (boxes[:, 3] / 2.0)
-    boxes[:, 2] = boxes[0] + boxes[:, 2]
-    boxes[:, 3] = boxes[1] + boxes[:, 3]
+    boxes[:, 0] = boxes[0] - (boxes[:, 2] / 2.0) # x_min
+    boxes[:, 1] = boxes[1] - (boxes[:, 3] / 2.0) # y_min
+    boxes[:, 2] = boxes[0] + boxes[:, 2]         # x_max
+    boxes[:, 3] = boxes[1] + boxes[:, 3]         # y_max 
+    return boxes
+
+def xyxy2n(boxes:np.ndarray, width:float, height:float)-> np.ndarray:
+    assert boxes.shape[-1] == 4
+    assert len(boxes.shape) == 2
+    return xyscaling(boxes, (1/width), (1/height))
+    
+def cxcywh2n(boxes:np.ndarray, width:float, height:float)->np.ndarray:
+    assert boxes.shape[-1] == 4
+    assert len(boxes.shape) == 2
+    return xyscaling(boxes, (1/width), (1/height))
+    
+def n2xyxy(boxes:np.ndarray, width:float, height:float)->np.ndarray:
+    assert boxes.shape[-1] == 4
+    assert len(boxes.shape) == 2
+    return xyscaling(boxes, width, height)
+
+def n2cxcy(boxes:np.ndarray, width:float, height:float)->np.ndarray:
+    assert boxes.shape[-1] == 4
+    assert len(boxes.shape) == 2
+    return xyscaling(boxes, width, height)
+
+def xyscaling(boxes:np.ndarray, width_scale:float, height_scale:float)->np.ndarray:
+    boxes[:, 0] = boxes[:, 0] * width_scale
+    boxes[:, 1] = boxes[:, 1] * height_scale
+    boxes[:, 2] = boxes[:, 2] * width_scale
+    boxes[:, 3] = boxes[:, 3] * height_scale
     return boxes
 
 class BoxTrans:
